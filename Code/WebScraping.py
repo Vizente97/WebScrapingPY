a5a9744f6bcb78f869325353f4e75277a6010498
Vizente97 <vizente14@gmail.com>
2020-12-17 15:46:12 -0600

    Cambio a Selenium

--- Code/WebScraping.py
+++ Code/WebScraping.py
@@ -1,9 +1,22 @@
-import requests
-import os
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys 
+import time
 
-contenido = requests.get("https://es.python-requests.org/es/latest/user/quickstart.html")
-print (contenido.headers)
+class unittest_prueba(unittest.TestCase):
 
-file = open("ArchivoHTML.txt", "w")
-file.write(str(contenido.content, 'utf-8'))
-file.close()
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path=r"C:\chromedriver\chromedriver.exe")         
+
+    def test_buscar(self):
+        driver = self.driver
+        driver.get("https://es.wikipedia.org/wiki/Web_scraping")
+        self.assertIn("scraping",driver.title)
+        time.sleep(5)
+        assert "No se encontro el elemento:" not in driver.page_source
+
+    def closeprogram(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
